3d50b38ef1d76b64194b0d554b95df2cd5cb46fb
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2011-09-27 15:54:07 +0000

    first files

---
+++ src/opendial/imports/XMLErrorHandler.java
@@ -0,0 +1,48 @@
+// =================================================================                                                                   
+// Copyright (C) 2009-2011 Pierre Lison (plison@ifi.uio.no)                                                                            
+//                                                                                                                                     
+// This library is free software; you can redistribute it and/or                                                                       
+// modify it under the terms of the GNU Lesser General Public License                                                                  
+// as published by the Free Software Foundation; either version 2.1 of                                                                 
+// the License, or (at your option) any later version.                                                                                 
+//                                                                                                                                     
+// This library is distributed in the hope that it will be useful, but                                                                 
+// WITHOUT ANY WARRANTY; without even the implied warranty of                                                                          
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                    
+// Lesser General Public License for more details.                                                                                     
+//                                                                                                                                     
+// You should have received a copy of the GNU Lesser General Public                                                                    
+// License along with this program; if not, write to the Free Software                                                                 
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                                                                           
+// 02111-1307, USA.                                                                                                                    
+// =================================================================                                                                   
+
+package opendial.imports;
+
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Error handler for XML syntax errors.
+ *
+ * @author  Pierre Lison (plison@ifi.uio.no)
+ * @version $Date:: 2011-09-27 17:54:53 #$
+ *
+ */
+public class XMLErrorHandler extends DefaultHandler {
+
+	public void error (SAXParseException e) { 
+		System.out.println("Parsing error: "+e.getMessage());
+	}
+	
+	public void warning (SAXParseException e) { 
+		System.out.println("Parsing problem: "+e.getMessage());
+	}
+	
+	public void fatalError (SAXParseException e) { 
+		System.out.println("Parsing error: "+e.getMessage()); 
+		System.out.println("Cannot continue."); 
+		System.exit(1);
+	}
+	
+}
--- testing/opendial/imports/XMLDomainReaderTest.java
+++ testing/opendial/imports/XMLDomainReaderTest.java
@@ -17,65 +17,28 @@
 // 02111-1307, USA.                                                                                                                    
 // =================================================================                                                                   
 
-package opendial.common.io;
+package opendial.imports;
 
-import java.io.IOException;
-import java.util.logging.Logger;
+import static org.junit.Assert.*;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.SchemaFactory;
-
-import opendial.common.utils.BasicConsoleLogger;
-
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import org.junit.Test;
+ 
 
 /**
- * XML reader utility for specification of dialogue domains.
+ * 
  *
  * @author  Pierre Lison (plison@ifi.uio.no)
  * @version $Date::                      $
  *
  */
+public class XMLDomainReaderTest {
 
-public class XMLDomainReader {
-
-	public static final String dialDomain = "domains//testing//microdom1.xml"; 
-	public static final String dialDomainSchema = "resources//domaindef.xsd";
-	
-	static Logger logger = BasicConsoleLogger.getDefaultLogger();
-
-	/** 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-	
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-		try {
-			SchemaFactory schema = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
-			factory.setSchema(schema.newSchema(new Source[] {new StreamSource(dialDomainSchema)}));
-			DocumentBuilder builder = factory.newDocumentBuilder();
-
-			builder.setErrorHandler(new XMLErrorHandler());
-			Document document = builder.parse(new InputSource(dialDomain));
-			logger.info("XML parsing of file: " + dialDomain + " successful!");
-
-		}
-		 catch (SAXException e) {
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        
+	@Test
+	public void test1() {
+		assertTrue(true);
 	}
-
+	
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
 }
