0850cc74c3d27f742a6efaeae2288b628e96a063
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2011-09-27 15:39:02 +0000

    first files

---
+++ src/opendial/common/io/XMLDomainReader.java
@@ -0,0 +1,81 @@
+// =================================================================                                                                   
+// Copyright (C) 2009-2011 Pierre Lison (plison@ifi.uio.no)                                                                            
+//                                                                                                                                     
+// This library is free software; you can redistribute it and/or                                                                       
+// modify it under the terms of the GNU Lesser General Public License                                                                  
+// as published by the Free Software Foundation; either version 2.1 of                                                                 
+// the License, or (at your option) any later version.                                                                                 
+//                                                                                                                                     
+// This library is distributed in the hope that it will be useful, but                                                                 
+// WITHOUT ANY WARRANTY; without even the implied warranty of                                                                          
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                    
+// Lesser General Public License for more details.                                                                                     
+//                                                                                                                                     
+// You should have received a copy of the GNU Lesser General Public                                                                    
+// License along with this program; if not, write to the Free Software                                                                 
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                                                                           
+// 02111-1307, USA.                                                                                                                    
+// =================================================================                                                                   
+
+package opendial.common.io;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+
+import opendial.common.utils.BasicConsoleLogger;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * XML reader utility for specification of dialogue domains.
+ *
+ * @author  Pierre Lison (plison@ifi.uio.no)
+ * @version $Date::                      $
+ *
+ */
+
+public class XMLDomainReader {
+
+	public static final String dialDomain = "domains//testing//microdom1.xml"; 
+	public static final String dialDomainSchema = "resources//domaindef.xsd";
+	
+	static Logger logger = BasicConsoleLogger.getDefaultLogger();
+
+	/** 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try {
+			SchemaFactory schema = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+			factory.setSchema(schema.newSchema(new Source[] {new StreamSource(dialDomainSchema)}));
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			builder.setErrorHandler(new XMLErrorHandler());
+			Document document = builder.parse(new InputSource(dialDomain));
+			logger.info("XML parsing of file: " + dialDomain + " successful!");
+
+		}
+		 catch (SAXException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+	}
+
+}
---
+++ src/opendial/common/utils/BasicConsoleLogger.java
@@ -0,0 +1,74 @@
+// =================================================================                                                                   
+// Copyright (C) 2009-2011 Pierre Lison (plison@ifi.uio.no)                                                                            
+//                                                                                                                                     
+// This library is free software; you can redistribute it and/or                                                                       
+// modify it under the terms of the GNU Lesser General Public License                                                                  
+// as published by the Free Software Foundation; either version 2.1 of                                                                 
+// the License, or (at your option) any later version.                                                                                 
+//                                                                                                                                     
+// This library is distributed in the hope that it will be useful, but                                                                 
+// WITHOUT ANY WARRANTY; without even the implied warranty of                                                                          
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                    
+// Lesser General Public License for more details.                                                                                     
+//                                                                                                                                     
+// You should have received a copy of the GNU Lesser General Public                                                                    
+// License along with this program; if not, write to the Free Software                                                                 
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                                                                           
+// 02111-1307, USA.                                                                                                                    
+// =================================================================                                                                   
+
+package opendial.common.utils;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+/**
+ * Utility for logging on the standard output (console).  
+ *
+ * @author  Pierre Lison (plison@ifi.uio.no)
+ * @version $Date::                      $
+ *
+ */
+public class BasicConsoleLogger extends Logger {
+
+	protected BasicConsoleLogger(String name, String resourceBundleName) {
+		super(name, resourceBundleName);
+	}
+
+	public static Logger getDefaultLogger() {
+
+		Logger l = getLogger("");
+
+		for (Handler handler2 : l.getHandlers()) {
+			l.removeHandler(handler2);
+		}
+
+		ConsoleHandler handler = new ConsoleHandler();
+		handler.setFormatter(new LogFormatter());
+		l.addHandler(handler);
+
+		return l;
+	}
+
+}
+
+
+/**
+ * String formatter associated with the console logger
+ * 
+ *
+ * @author  Pierre Lison (plison@ifi.uio.no)
+ * @version $date::                      $
+ *
+ */
+final class LogFormatter extends Formatter {
+
+	@Override
+	public String format(LogRecord arg0) {
+		return "["+arg0.getClass().getSimpleName()+"] " + arg0.getLevel() + ": " + arg0.getMessage();
+	}
+}
+
