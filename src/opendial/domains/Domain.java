2097964c8fd5b93ec9b2cd79b9db8749adbcbe69
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2011-09-30 16:13:25 +0000

    domain-related objects

---
+++ src/opendial/domains/Domain.java
@@ -0,0 +1,107 @@
+// =================================================================                                                                   
+// Copyright (C) 2009-2011 Pierre Lison (plison@ifi.uio.no)                                                                            
+//                                                                                                                                     
+// This library is free software; you can redistribute it and/or                                                                       
+// modify it under the terms of the GNU Lesser General Public License                                                                  
+// as published by the Free Software Foundation; either version 2.1 of                                                                 
+// the License, or (at your option) any later version.                                                                                 
+//                                                                                                                                     
+// This library is distributed in the hope that it will be useful, but                                                                 
+// WITHOUT ANY WARRANTY; without even the implied warranty of                                                                          
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                    
+// Lesser General Public License for more details.                                                                                     
+//                                                                                                                                     
+// You should have received a copy of the GNU Lesser General Public                                                                    
+// License along with this program; if not, write to the Free Software                                                                 
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                                                                           
+// 02111-1307, USA.                                                                                                                    
+// =================================================================                                                                   
+
+package opendial.domains;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import opendial.domains.actions.Action;
+import opendial.domains.observations.Observation;
+import opendial.state.DialogueState;
+import opendial.utils.Logger;
+
+/**
+ * Representation of a dialogue domain, complete with entities and variables,
+ * rule-based probabilistic models, observations and actions.
+ *
+ * @author  Pierre Lison (plison@ifi.uio.no)
+ * @version $Date::                      $
+ *
+ */
+public class Domain {
+
+	static Logger log = new Logger("DialogueDomain", Logger.Level.NORMAL);
+	
+	// name of dialogue domain
+	String domainName;
+	
+	// entity declarations
+	Map<String,EntityType> entityTypes;
+	
+	// initial state of the domain
+	DialogueState initialState;
+	
+	// the specified models (list of rules)
+	Map<Model.Type,Model> models;
+	
+	// the possible observations for the domain
+	List<Observation> observations;
+	
+	// the possible actions for the domain
+	List<Action> actions;
+	
+	/**
+	 * Create a (empty) dialogue domain
+	 * 
+	 * @param domainName name of dialogue domain (can be empty string)
+	 */
+	public Domain(String domainName) {
+		this.domainName = domainName;
+		entityTypes = new HashMap<String,EntityType>();
+		initialState = new DialogueState();
+		models = new HashMap<Model.Type, Model>();
+		observations = new ArrayList<Observation>();
+		actions = new ArrayList<Action>();
+	}
+	
+	
+	public void addEntityType(EntityType newEntity) {
+		entityTypes.put(newEntity.getName(), newEntity);
+	}
+
+
+	public void addEntityTypes(List<EntityType> entityTypes) {
+		for (EntityType e: entityTypes){
+			addEntityType(e);
+		}
+	}
+	
+	
+	public void addModel(Model model) {
+		models.put(model.getType(), model);
+	}
+	
+	public void addObservation(Observation obs) {
+		observations.add(obs);
+	}
+	
+	public void addAction(Action action) {
+		actions.add(action);
+	}
+
+	
+	public Collection<EntityType> getEntityTypes() {
+		return entityTypes.values();
+	}
+
+}
---
+++ src/opendial/domains/rules/conditions/BasicCondition.java
@@ -0,0 +1,55 @@
+// =================================================================                                                                   
+// Copyright (C) 2009-2011 Pierre Lison (plison@ifi.uio.no)                                                                            
+//                                                                                                                                     
+// This library is free software; you can redistribute it and/or                                                                       
+// modify it under the terms of the GNU Lesser General Public License                                                                  
+// as published by the Free Software Foundation; either version 2.1 of                                                                 
+// the License, or (at your option) any later version.                                                                                 
+//                                                                                                                                     
+// This library is distributed in the hope that it will be useful, but                                                                 
+// WITHOUT ANY WARRANTY; without even the implied warranty of                                                                          
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                    
+// Lesser General Public License for more details.                                                                                     
+//                                                                                                                                     
+// You should have received a copy of the GNU Lesser General Public                                                                    
+// License along with this program; if not, write to the Free Software                                                                 
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                                                                           
+// 02111-1307, USA.                                                                                                                    
+// =================================================================                                                                   
+
+package opendial.domains.rules.conditions;
+
+import opendial.domains.rules.Variable;
+import opendial.utils.Logger;
+
+/**
+ * 
+ *
+ * @author  Pierre Lison (plison@ifi.uio.no)
+ * @version $Date::                      $
+ *
+ */
+public class BasicCondition extends Condition {
+
+	static Logger log = new Logger("BasicCondition", Logger.Level.NORMAL);
+	
+	public static enum Relation {EQUAL, UNEQUAL, GREATER_THAN, LESSER_THAN}
+	
+	Variable var;
+	
+	String value;
+	
+	Relation rel = Relation.EQUAL;
+	
+	public BasicCondition (Variable var, String value) {
+		this.var = var;
+		this.value = value;
+	}
+	
+	public BasicCondition (Variable var, String value, Relation rel) {
+		this(var,value);
+		this.rel = rel;
+	}
+	
+	
+}
