edf414fb7351c9e479a9605f8452ffd899b6baee
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-02-14 13:17:20 +0000

    more debugging

--- src/opendial/simulation/UserSimulator.java
+++ src/opendial/simulation/UserSimulator.java
@@ -150,13 +150,19 @@ public class UserSimulator extends Thread {
 
 			nbTurns++;
 			
-			if (nbTurns == 20) {
+			if (nbTurns == 10) {
 				log.debug("===> estimate for theta_1: " + systemState.getContent("theta_1", true));
 				log.debug("===> estimate for theta_2: " + systemState.getContent("theta_2", true));
 				log.debug("===> estimate for theta_3: " + systemState.getContent("theta_3", true));
 				log.debug("===> estimate for theta_4: " + systemState.getContent("theta_4", true));
 				log.debug("===> estimate for theta_5: " + systemState.getContent("theta_5", true));
 				log.debug("===> estimate for theta_6: " + systemState.getContent("theta_6", true));
+				log.debug("===> estimate for theta_7: " + systemState.getContent("theta_7", true));
+				log.debug("===> estimate for theta_8: " + systemState.getContent("theta_8", true));
+				log.debug("===> estimate for theta_9: " + systemState.getContent("theta_9", true));
+				log.debug("===> estimate for theta_10: " + systemState.getContent("theta_10", true));
+				log.debug("===> estimate for theta_11: " + systemState.getContent("theta_11", true));
+				log.debug("===> estimate for theta_12: " + systemState.getContent("theta_12", true));
 				nbTurns = 0;
 			}
 			
