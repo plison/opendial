ef8f2578249bbe9f84bb4448a068e4af2f7f503e
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-02-19 21:12:49 +0000

    more debugging

--- trunk/src/opendial/arch/Settings.java
+++ trunk/src/opendial/arch/Settings.java
@@ -64,10 +64,10 @@ public class Settings {
 	
 	public Class<? extends InferenceAlgorithm> inferenceAlgorithm = SwitchingAlgorithm.class;;
 	
-	public int nbSamples = 3000;
+	public int nbSamples = 4000;
 	
 	// maximum sampling time (in milliseconds)
-	public long maximumSamplingTime = 1500;
+	public long maximumSamplingTime = 2000;
 	
 	public int nbDiscretisationBuckets = 100;
 			 
