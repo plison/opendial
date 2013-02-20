a2f1ef275dc1efa4d7e744511cb93b9775030c54
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-02-20 07:47:42 +0000

    more debugging

--- trunk/src/opendial/bn/distribs/continuous/functions/ProductKernelDensityFunction.java
+++ trunk/src/opendial/bn/distribs/continuous/functions/ProductKernelDensityFunction.java
@@ -147,7 +147,7 @@ public class ProductKernelDensityFunction implements MultivariateDensityFunction
 			double sum = 0;
 				for (int i = 0 ; i < newPoint.length; i++) {
 					while (newPoint[i] <= 0) {
-						newPoint[i] = new GaussianDensityFunction(point[i], bandwidths[i]/40.0).sample();
+						newPoint[i] = new GaussianDensityFunction(point[i], bandwidths[i]/1.0).sample();
 					}
 					sum += newPoint[i];
 				}
