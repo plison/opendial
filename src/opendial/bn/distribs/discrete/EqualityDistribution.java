ad0b9b586690e9235826cfec6b25e35791d65a28
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-02-22 21:49:40 +0000

    more debugging

--- src/opendial/bn/distribs/discrete/EqualityDistribution.java
+++ src/opendial/bn/distribs/discrete/EqualityDistribution.java
@@ -48,8 +48,8 @@ public class EqualityDistribution implements DiscreteProbDistribution {
 
 	Random sampler;
 
-	public static final double PROB_WITH_SINGLE_NONE = 0.15;
-	public static final double PROB_WITH_DOUBLE_NONE = 0.15;
+	public static final double PROB_WITH_SINGLE_NONE = 0.12;
+	public static final double PROB_WITH_DOUBLE_NONE = 0.12;
 
 
 	public EqualityDistribution(String equalityId, String variable) {
