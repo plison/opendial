135f339ae6a225e0c7cbb2e6bafdf145a2647505
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-09-15 13:57:54 +0000

    new version for experiments

--- src/opendial/bn/nodes/UtilityRuleNode.java
+++ src/opendial/bn/nodes/UtilityRuleNode.java
@@ -57,7 +57,7 @@ public class UtilityRuleNode extends UtilityNode {
 
 	AnchoredRule rule;
 	
-	public static final double LIKELY_VALUE_THRESHOLD = 0.2;
+	public static final double LIKELY_VALUE_THRESHOLD = 0.1;
 	
 
 	public UtilityRuleNode(AnchoredRule rule) throws DialException {
