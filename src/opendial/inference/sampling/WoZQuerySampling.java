14f680b6487cb8895e961fabe5a5ed70e72480d6
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-09-17 17:25:44 +0000

    after much debugging

--- src/opendial/inference/sampling/WoZQuerySampling.java
+++ src/opendial/inference/sampling/WoZQuerySampling.java
@@ -40,7 +40,7 @@ import opendial.inference.queries.UtilQuery;
 
 public class WoZQuerySampling extends AbstractQuerySampling {
 
-	public static final double MAX = 40;
+	public static final double MAX = 60;
 	
 	// logger
 	public static Logger log = new Logger("WoZQuerySampling",
