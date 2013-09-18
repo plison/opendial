b833471bfe6599a49a1d0659f7c20f34faab7a36
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-09-18 19:57:41 +0000

    trying to converge to a stable behaviour

--- src/opendial/inference/sampling/WoZQuerySampling.java
+++ src/opendial/inference/sampling/WoZQuerySampling.java
@@ -40,9 +40,9 @@ import opendial.inference.queries.UtilQuery;
 
 public class WoZQuerySampling extends AbstractQuerySampling {
 
-	public static final double RATE = 50;
+	public static final double RATE = 60;
 	public static final double MIN = -20;
-	public static final double MAX = 40;
+	public static final double MAX = 30;
 	
 	// logger
 	public static Logger log = new Logger("WoZQuerySampling",
