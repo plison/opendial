f40a38d70a93acdbc6c435931f6eb082b811590c
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2013-09-18 06:48:26 +0000

    after much debugging

--- trunk/src/opendial/inference/sampling/WoZQuerySampling.java
+++ trunk/src/opendial/inference/sampling/WoZQuerySampling.java
@@ -40,7 +40,7 @@ import opendial.inference.queries.UtilQuery;
 
 public class WoZQuerySampling extends AbstractQuerySampling {
 
-	public static final double MAX = 50;
+	public static final double MAX = 80;
 	
 	// logger
 	public static Logger log = new Logger("WoZQuerySampling",
@@ -105,12 +105,12 @@ public class WoZQuerySampling extends AbstractQuerySampling {
 				Assignment action = sample.getSample().getTrimmed(goldAction.getVariables());
 				if (action.equals(goldAction)  && sample.getUtility() <= averages.get(bestNotGold)) {
 					double distance = averages.get(bestNotGold) - sample.getUtility();
-					double distance2 = Math.max(MAX/2, distance);
+					double distance2 = Math.max(MAX/4, distance);
 						weight *= Math.abs(MAX - distance2) / MAX;
 				}
 				else if (!action.equals(goldAction) && sample.getUtility() >= averages.get(goldAction)) {
 					double distance = sample.getUtility() - averages.get(goldAction);
-					double distance2 = Math.max(MAX/2, distance);
+					double distance2 = Math.max(MAX/4, distance);
 					double factor = (goldAction.isDefault())? 0.3 : 1.0;
 					weight *= Math.abs(MAX - (factor * distance2 / averages.size())) / MAX;
 				}
