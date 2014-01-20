e4151feeb31c18005cfe6b2578ec96d96772818e
plison2@gmail.com <plison2@gmail.com@f8a60ef7-fc7a-9e06-e654-4a37d5fbcd52>
2014-01-20 14:32:39 +0000

    icon

--- src/opendial/modules/Simulator.java
+++ src/opendial/modules/Simulator.java
@@ -210,6 +210,14 @@ public class Simulator implements Module {
 	}
 
 
+	/**
+	 * Generates new simulated observations and adds them to the dialogue state. The 
+	 * method returns true when a new user input has been generated, and false 
+	 * otherwise.
+	 * 
+	 * @return whether a user input has been generated
+	 * @throws DialException
+	 */
 	private boolean addNewObservations() throws DialException {
 		List<String> newObsVars = new ArrayList<String>();
 		for (String var : simulatorState.getChanceNodeIds()) {
