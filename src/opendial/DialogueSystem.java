59baa701dfdb97e4428bfe2c425c994f2926b447
Pierre Lison <plison@ifi.uio.no>
2015-03-25 21:02:45 +0100

    adding last modified date in source

--- src/opendial/DialogueSystem.java
+++ src/opendial/DialogueSystem.java
@@ -68,7 +68,7 @@ import opendial.state.DialogueState;
  *  method startSystem(). The system can be paused or resumed at any time. 
  *
  * @author  Pierre Lison (plison@ifi.uio.no)
- * @version $Date::                      $
+ * @version ${date}
  *
  */
 public class DialogueSystem {
