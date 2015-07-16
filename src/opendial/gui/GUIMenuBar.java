e35f82a114ec59851461f541c3851a45c4fe930b
Pierre Lison <plison@ifi.uio.no>
2015-07-16 16:52:42 +0200

    adding tests for the Malt parser

--- src/opendial/gui/GUIMenuBar.java
+++ src/opendial/gui/GUIMenuBar.java
@@ -391,7 +391,7 @@ public class GUIMenuBar extends JMenuBar {
 			style.append("font-size:" + font.getSize() + "pt;");
 
 			JEditorPane ep = new JEditorPane("text/html", "<html><body style=\""
-					+ style + "\"><b>OpenDial dialogue toolkit, version 1.3</b><br>"
+					+ style + "\"><b>OpenDial dialogue toolkit, version 1.4</b><br>"
 					+ "Copyright (C) 2011-2015 by Pierre Lison<br>University of Oslo, Norway<br><br>"
 					+ "OpenDial is distributed as free software under<br>"
 					+ "the <a href=\"http://opensource.org/licenses/MIT\">MIT free software license</a>.<br><br>"
